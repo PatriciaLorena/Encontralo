e7a43bc012033558296be029271ea02b9fb7af85
Paty <patrilorenfer@gmail.com>
2020-10-15 18:15:09 -0300

    administracion de roles y usuarios

--- empresa/app/Http/Controllers/Auth/RegisterController.php
+++ empresa/app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use App\Role;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,10 +65,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+      $user = User::create([
+          'name' => $data['name'],
+          'email' => $data['email'],
+          'password' => bcrypt($data['password']),
+      ]);
+
+      $user
+          ->roles()
+          ->attach(Role::where('name', 'user')->first());
+      return $user;
     }
 }
