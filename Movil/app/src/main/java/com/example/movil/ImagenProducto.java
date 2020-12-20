4b90ba89bc4e7f3f36902c49a343bce6ba158ae9
Paty <patrilorenfer@gmail.com>
2020-12-20 01:01:42 -0300

    boton buscar con estilos

--- Movil/app/src/main/java/com/example/movil/ImagenProducto.java
+++ Movil/app/src/main/java/com/example/movil/ImagenProducto.java
@@ -10,6 +10,6 @@ public class ImagenProducto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagen_producto);
-        
+
     }
 }
