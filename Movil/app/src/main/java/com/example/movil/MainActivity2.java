28b9b3daed0caa97b886d568d731bf10bb8e31bb
Paty <patrilorenfer@gmail.com>
2021-03-16 22:55:05 -0300

    mostrando informacion del producto

--- Movil/app/src/main/java/com/example/movil/MainActivity2.java
+++ Movil/app/src/main/java/com/example/movil/MainActivity2.java
@@ -7,12 +7,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.movil.model.Article;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity2 extends AppCompatActivity {
     private ImageView imgItemDetail;
     private TextView tvNombreDetail;
     private TextView tvDescripcionDetail;
     private Article itemDetail;
+
+    private String domain_image = "http://192.168.0.14:8000/imagenes/articulos/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,11 @@ public class MainActivity2 extends AppCompatActivity {
     public void initValues(){
         itemDetail = (Article) getIntent().getExtras().getSerializable("itemDetail");
 
-        imgItemDetail.setImageResource(Integer.parseInt(itemDetail.getImagen()));
+        //imgItemDetail.setImageURI(itemDetail.getImagen());
+
+        Picasso.get()
+                .load(domain_image+itemDetail.getImagen())
+                .into(this.imgItemDetail);
         tvNombreDetail.setText(itemDetail.getNombre());
         tvDescripcionDetail.setText(itemDetail.getDescripcion());
     }
