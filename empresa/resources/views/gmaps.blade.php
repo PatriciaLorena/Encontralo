9787c702517cd1ddbb3edf9c5cf44296a1ca0856
Paty <patrilorenfer@gmail.com>
2023-07-18 20:34:27 -0400

    eliminacion de archivos innecesarios y mostrar coordenadas y mapa con la posicion del usuario

--- empresa/resources/views/gmaps.blade.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-    <script type="text/javascript">var centreGot = false;</script>{!!$map['js']!!}
-</head>
-<body>
-    {!!$map['html']!!}
-</body>
-</html>
--- resources/views/empresa/create.blade.php
+++ resources/views/empresa/create.blade.php
@@ -11,7 +11,7 @@
 		</div>
 	@endif
 <div class="row">
-  <h1>Agregar Empresa</h1>
+  <h1>Agregarrrrr Empresa</h1>
     <form method="POST" action="{{route("empresa.store")}}">
       @csrf
 </div>
@@ -117,6 +117,89 @@
   </div>
 
 
+  <script>
+
+
+var marker;          //variable del marcador
+var coords = {};    //coordenadas obtenidas con la geolocalización
+
+//Funcion principal
+initMap = function ()
+{
+
+    //usamos la API para geolocalizar el usuario
+        navigator.geolocation.getCurrentPosition(
+          function (position){
+            coords =  {
+              lng: position.coords.longitude,
+              lat: position.coords.latitude
+            };
+            setMapa(coords);  //pasamos las coordenadas al metodo para crear el mapa
+            //console.log(coords)
+            //carga las coordenadas en el input con id coords
+            document.getElementById("latitud").value = coords.lat;
+            document.getElementById("longitud").value = coords.lng;
+
+          },function(error){console.log(error);});
+
+}
+
+
+
+function setMapa (coords)
+{
+      //Se crea una nueva instancia del objeto mapa
+      var map = new google.maps.Map(document.getElementById('mapa'),
+
+
+
+      {
+        zoom: 15,
+        center:new google.maps.LatLng(coords.lat,coords.lng),
+
+      });
+
+      //Creamos el marcador en el mapa con sus propiedades
+      //para nuestro obetivo tenemos que poner el atributo draggable en true
+      //position pondremos las mismas coordenas que obtuvimos en la geolocalización
+      marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: new google.maps.LatLng(coords.lat,coords.lng),
+
+      });
+      //agregamos un evento al marcador junto con la funcion callback al igual que el evento dragend que indica
+      //cuando el usuario a soltado el marcador
+      marker.addListener('click', toggleBounce);
+
+      marker.addListener( 'dragend', function (event)
+      {
+        //escribimos las coordenadas de la posicion actual del marcador dentro del input #coords
+       // document.getElementById("coords").value = this.getPosition().lat()+","+ this.getPosition().lng();
+       
+       document.getElementById("latitud").value = this.getPosition().lat();
+       document.getElementById("longitud").value = this.getPosition().lng();
+
+      });
+}
+
+//callback al hacer clic en el marcador lo que hace es quitar y poner la animacion BOUNCE
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+// Carga de la libreria de google maps
+
+    </script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?callback=initMap"></script>
+
+
+<!--
 <script type="text/javascript">
 
   function iniciarMapa(){
@@ -154,6 +237,8 @@
 
 <script src="https://maps.googleapis.com/maps/api/js?Key=AIzaSyCo4LRSwMXoWZhaq-7YmGBhggYihH4ZzZE&callback=iniciarMapa">
  </script>
+-->
+ 
 
       <div class="form-group row mb-0">
         <div class="col-md-8 offset-md-4">
