9d838d88f8426d8c30a280692f35d9a93a5e169f
Paty <patrilorenfer@gmail.com>
2023-07-21 20:05:45 -0400

    delete from file and  mostrar mediante api productos con el nombre y coords de la empresa relacionadas

--- app/Http/Controllers/Api/articuloController.php
+++ app/Http/Controllers/Api/articuloController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Articulo;
+use App\Empresa;
 
 class articuloController extends Controller
 {
     function all(){
-      $articulo = Articulo::all();
-      return $articulo;
+      $articulos = Articulo::join('empresas', 'articulos.idEmpresa', '=', 'empresas.idEmpresa')
+      ->select('articulos.*', 'empresas.nombreEmpresa', 'empresas.latitud', 'empresas.longitud')
+      ->get();
+      return $articulos;
     }
 }
+
